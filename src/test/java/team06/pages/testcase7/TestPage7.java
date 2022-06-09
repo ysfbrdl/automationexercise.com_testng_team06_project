5e3d018351aa06ab912f26b928023bac448bcc01
Emin Halil <emin_halil@abv.bg>
2022-06-09 23:19:48 +0300

    emin deneme 09/06/22

--- src/test/java/team06/pages/testcase7/TestPage7.java
+++ src/test/java/team06/pages/testcase7/TestPage7.java
@@ -1,4 +1,18 @@
 package team06.pages.testcase7;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import team06.utilities.Driver;
+
 public class TestPage7 {
+
+    public TestPage7(){PageFactory.initElements(Driver.getDriver(),this);
+
+    }
+    @FindBy(linkText = "Test Cases")
+    public WebElement testCasesLink;
+
+
+
 }
--- src/test/java/team06/tests/TestCase7.java
+++ src/test/java/team06/tests/TestCase7.java
@@ -1,4 +1,29 @@
 package team06.tests;
 
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import team06.pages.testcase7.TestPage7;
+import team06.utilities.ConfigReader;
+import team06.utilities.Driver;
+
 public class TestCase7 {
+    TestPage7 testPage7;
+
+    @Test
+    public void testCase7(){
+        Driver.getDriver().get(ConfigReader.getProperty("automation_exercise_url"));
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(),"https://www.automationexercise.com/");
+        testPage7 = new TestPage7();
+        testPage7.testCasesLink.click();
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(),"https://www.automationexercise.com/test_cases");
+
+        Driver.closeDriver();
+
+
+
+    }
+
+
+
 }
